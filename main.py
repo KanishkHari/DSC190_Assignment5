39ddd0bb71c4052f7df94d4867d449e6b471d28d
KanishkHari <kanishkhari0@gmail.com>
2026-05-03 22:41:48 -0700

    Fix lint errors in main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import os
-import sys  # unused import
+def hello():
+  """Say Hello"""
+  print("Hello")
 
-x=1+2  # missing spaces around operator
-def hello( ):  # extra space before parenthesis
-    """Say hello."""
-    print("hello")
-    unused_var = 42  # unused variable
-
-# Line way too long (E501) - this line exceeds the character limit that ruff enforces
-message = "This is an extremely long line that definitely exceeds the maximum line length that the linter is configured to check"
-
-hello()
+  hello()
